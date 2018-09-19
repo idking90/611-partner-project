21c4f91925e40c85896cbd1f84f319656020ed3b
idking <idking@COBD0WXD.Busdom.ColoState.EDU>
2018-09-19 14:53:58 -0600

    Solves both mazes we have.
    Not positive how it would handle an obstacle on the far left column, far
    right column, or the bottom row. I'm not 100% sure that's something we
    need to address, though

--- src/Direction.java
+++ src/Direction.java
@@ -5,8 +5,8 @@ public class Direction extends Thread{
 
 	Maze maze;
 	Position location;
-	/*Boolean outcome;
-	Boolean mazeDone;*/
+	Boolean legalMove;
+	Boolean mazeDone;
 	int maxRows;
 	int maxColumns;
 	int currRow;
@@ -66,8 +66,94 @@ public class Direction extends Thread{
 		if (mazeDone) {
 		location.textArea.append("Logo Found \n");}*/
 		
+		
+		
+//************ this is where Part II code mostly begins (aside from a few variables)***************		
 		maxRows = maze.getHeight();
 		maxColumns = maze.getWidth();
+		
+		//master loop, when maze.isDone() returns true, should skip the loop
+		while (!maze.isDone()) {
+			currCol = maze.getCurrCol();
+			currRow = maze.getCurrRow();
+			if (currCol == 0 | (currCol % 2) == 0) { //even row
+				while (currCol < (maxColumns - 1)) { //keep moving until hits far right column
+					if(!maze.moveRight()) {//obstacle in way, must go down-right-right-up
+						maze.moveDown();
+						mazeDone = maze.isDone();
+						if(mazeDone) {break;}
+						maze.moveRight();
+						mazeDone = maze.isDone();
+						if(mazeDone) {break;}
+						maze.moveRight();
+						mazeDone = maze.isDone();
+						if(mazeDone) {break;}
+						maze.moveUp();
+						mazeDone = maze.isDone();
+						if(mazeDone) {break;}
+				}
+				else {
+					legalMove = true;
+					mazeDone = maze.isDone();
+					location.textArea.append("Legal move? " + legalMove + "\n" + "Maze Done?" + mazeDone + "\n");
+					currCol = maze.getCurrCol();
+					if(mazeDone) {break;}
+				}
+			}
+			
+			if(maze.isDone()) {break;}
+			}
+			else { //odd row
+				while (currCol > 0) { //keep moving until hits far left column
+					if(!maze.moveLeft()) {//obstacle in way, must go up-left-left-down
+							maze.moveUp();
+							mazeDone = maze.isDone();
+							if(mazeDone) {break;}
+							maze.moveLeft();
+							mazeDone = maze.isDone();
+							if(mazeDone) {break;}
+							maze.moveLeft();
+							mazeDone = maze.isDone();
+							if(mazeDone) {break;}
+							maze.moveDown();
+							mazeDone = maze.isDone();
+							if(mazeDone) {break;}
+					}
+					else {
+						legalMove = true;
+						mazeDone = maze.isDone();
+						location.textArea.append("Legal move? " + legalMove + "\n" + "Maze Done?" + mazeDone + "\n");
+						currCol = maze.getCurrCol();
+						if(mazeDone) {break;}
+					}
+				}
+				if(maze.isDone()) {break;}	
+			}
+			
+			if (currRow < (maxRows -1)) { //not at bottom of the maze
+				legalMove = maze.moveDown();
+				mazeDone = maze.isDone();
+				location.textArea.append("Legal move? " + legalMove + "\n" + "Maze Done?" + mazeDone + "\n");
+				currRow = maze.getCurrRow();
+				mazeDone = maze.isDone();
+				if(mazeDone) {break;}
+				}
+				else { location.textArea.append("Maze completion unsuccessful \n");
+				//reached bottom of maze and didn't find logo
+				}
+		
+		
+		
+		// this bracket ends the big While loop
+		}
+		
+		
+		if (maze.isDone()) {
+			location.textArea.append("Logo Found! YOU DID IT!!!!!!!!! \n");
+		}
+		else {
+			location.textArea.append("Maze completion unsuccessful \n");
+		}
 		
 		
 		
