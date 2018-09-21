271cc80b88e03678e8d9a48f433a008d5e849085
idking <idking@COBD0WXL.Busdom.ColoState.EDU>
2018-09-21 14:52:49 -0600

    Seems to work to me. I'll zip and submit these .java files

--- src/Direction.java
+++ src/Direction.java
@@ -1,7 +1,7 @@
 // This class must have the code in the run() method, sub-program/routain to slove the maze;
 //   seraches for the Java logo based on the provided path/direction algorithm
 
-public class Direction extends Thread{
+public class Direction extends Thread {
 
 	Maze maze;
 	Position location;
@@ -14,7 +14,6 @@ public class Direction extends Thread{
 	
 	
 	Direction(Maze maze, Position location) {
-		
 		this.maze = maze;
 		this.location = location;
 	}
@@ -29,138 +28,175 @@ public class Direction extends Thread{
 		// in the Position.java class, appending information into the JTextArea object
 		
 		/*while(!maze.isDone()) {
-		
-			mazeDone = maze.isDone();
+		  	mazeDone = maze.isDone();
 			location.textArea.append("Is the maze done?" + mazeDone + "\n");
-		for (int i = 1; i <9; i ++) {
-			outcome = this.maze.moveRight();
-			if (outcome) {
-			location.textArea.append("Sucess " + "\n");
-			location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				
+				for (int i = 1; i <9; i ++) {
+					outcome = this.maze.moveRight();
+					if (outcome) {
+					location.textArea.append("Success " + "\n");
+					location.textArea.append(" row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				}
+					else {location.textArea.append("Failure " + "\n");
+						break;
+					}	
+				}
+				
+				for (int i=1; i<4; i++) {
+					outcome = this.maze.moveDown();
+					if (outcome) {location.textArea.append("Success " + "\n");	
+					location.textArea.append(" row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+				}
+					else {location.textArea.append("Failure " + "\n");
+					break;
+		  			}
 			}
-			else {location.textArea.append("Failure " + "\n");
-				break;}
-			}
-		for (int i=1; i<4; i++) {
-			outcome = this.maze.moveDown();
-			if (outcome) {location.textArea.append("Sucess " + "\n");	
-			location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
-}
-			else {location.textArea.append("Failure " + "\n");
-				break;
-			}
-					}
+			
 		mazeDone = maze.isDone();
 		location.textArea.append("Is the maze done?" + mazeDone + "\n");
 		outcome = this.maze.moveLeft();
+		
 		if (outcome) {location.textArea.append("Sucess " + "\n");
-		location.textArea.append("Moved to row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
+			location.textArea.append(" row " + maze.getCurrRow() + ", column " + maze.getCurrCol() + "\n");
 		}
-		else {location.textArea.append("Failure " + "\n");
-			break;}
+			else {location.textArea.append("Failure " + "\n");
+				break;
+			}
 	
 		mazeDone = maze.isDone();
 		location.textArea.append("Is the maze done?" + mazeDone + "\n");
 			
 	}
 		if (mazeDone) {
-		location.textArea.append("Logo Found \n");}*/
-		
-		
+		location.textArea.append("Logo Found \n");
+		}*/
 		
 //************ this is where Part II code mostly begins (aside from a few variables)***************		
 		maxRows = maze.getHeight();
 		maxColumns = maze.getWidth();
+		currCol = maze.getCurrCol();
+		currRow = maze.getCurrRow();
+		
+		location.textArea.append("Start row " + currRow + ", column " + currCol + "\n");
 		
 		//master loop, when maze.isDone() returns true, should skip the loop
 		while (!maze.isDone()) {
-			currCol = maze.getCurrCol();
-			currRow = maze.getCurrRow();
 			if (currCol == 0 | (currCol % 2) == 0) { //even row
 				while (currCol < (maxColumns - 1)) { //keep moving until hits far right column
 					if(!maze.moveRight()) {//obstacle in way, must go down-right-right-up
 						maze.moveDown();
+						currRow = maze.getCurrRow();
+						location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
 						mazeDone = maze.isDone();
-						if(mazeDone) {break;}
+						if(mazeDone) { 
+							break;
+						}
 						maze.moveRight();
-						mazeDone = maze.isDone();
-						if(mazeDone) {break;}
-						maze.moveRight();
-						mazeDone = maze.isDone();
-						if(mazeDone) {break;}
-						maze.moveUp();
-						mazeDone = maze.isDone();
-						if(mazeDone) {break;}
-				}
-				else {
-					legalMove = true;
-					mazeDone = maze.isDone();
-					location.textArea.append("Legal move? " + legalMove + "\n" + "Maze Done?" + mazeDone + "\n");
-					currCol = maze.getCurrCol();
-					if(mazeDone) {break;}
-				}
-			}
-			
-			if(maze.isDone()) {break;}
-			}
-			else { //odd row
-				while (currCol > 0) { //keep moving until hits far left column
-					if(!maze.moveLeft()) {//obstacle in way, must go up-left-left-down
-							maze.moveUp();
-							mazeDone = maze.isDone();
-							if(mazeDone) {break;}
-							maze.moveLeft();
-							mazeDone = maze.isDone();
-							if(mazeDone) {break;}
-							maze.moveLeft();
-							mazeDone = maze.isDone();
-							if(mazeDone) {break;}
-							maze.moveDown();
-							mazeDone = maze.isDone();
-							if(mazeDone) {break;}
-					}
-					else {
-						legalMove = true;
-						mazeDone = maze.isDone();
-						location.textArea.append("Legal move? " + legalMove + "\n" + "Maze Done?" + mazeDone + "\n");
 						currCol = maze.getCurrCol();
-						if(mazeDone) {break;}
+						location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+						mazeDone = maze.isDone();
+						if(mazeDone) { 
+							break;
+						}
+						maze.moveRight();
+						currCol = maze.getCurrCol();
+						location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+						mazeDone = maze.isDone();
+						if(mazeDone) { 
+							break;
+						}
+						maze.moveUp();
+						currRow = maze.getCurrRow();
+						location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+						mazeDone = maze.isDone();
+						if(mazeDone) { 
+							break;
+						}
 					}
+						else {
+							legalMove = true;
+							mazeDone = maze.isDone();
+							currCol = maze.getCurrCol();
+							currRow = maze.getCurrRow();
+							location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+							if(mazeDone) {
+								break;
+							}
+						}
 				}
-				if(maze.isDone()) {break;}	
+				if(maze.isDone()) {
+					break;
+				}
 			}
+				else { //odd row
+					while (currCol > 0) { //keep moving until hits far left column
+						if(!maze.moveLeft()) {//obstacle in way, must go up-left-left-down
+							maze.moveUp();
+							currRow = maze.getCurrRow();
+							location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+							mazeDone = maze.isDone();
+							if(mazeDone) {
+								break;
+							}
+							maze.moveLeft();
+							currCol = maze.getCurrCol();
+							location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+							mazeDone = maze.isDone();
+							if(mazeDone) {
+								break;
+							}
+							maze.moveLeft();
+							currCol = maze.getCurrCol();
+							location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+							mazeDone = maze.isDone();
+							if(mazeDone) {
+								break;
+							}
+							maze.moveDown();
+							currRow = maze.getCurrRow();
+							location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+							mazeDone = maze.isDone();
+							if(mazeDone) {
+								break;
+							}
+						}
+							else {
+								legalMove = true;
+								mazeDone = maze.isDone();
+								currCol = maze.getCurrCol();
+								currRow = maze.getCurrRow();
+								location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
+								if(mazeDone) {
+									break;
+								}
+							}
+					}
+					if(maze.isDone()) {
+						break;
+					}	
+				}
 			
 			if (currRow < (maxRows -1)) { //not at bottom of the maze
 				legalMove = maze.moveDown();
-				mazeDone = maze.isDone();
-				location.textArea.append("Legal move? " + legalMove + "\n" + "Maze Done?" + mazeDone + "\n");
 				currRow = maze.getCurrRow();
+				location.textArea.append("Moved to row " + currRow + ", column " + currCol + "\n");
 				mazeDone = maze.isDone();
-				if(mazeDone) {break;}
-				}
-				else { location.textArea.append("Maze completion unsuccessful \n");
-				//reached bottom of maze and didn't find logo.
-		
-		
-		
+				
+					if(mazeDone) {
+						break;
+					}
+			}
+					else {
+						location.textArea.append("Maze completion unsuccessful. \n");
+					//reached bottom of maze and didn't find logo.
+					}
 		// this bracket below ends the big While loop. Most everything should be above this.
-		}
-		
-		
+	    }
 		if (maze.isDone()) {
 			location.textArea.append("Logo Found! YOU DID IT!!!!!!!!! \n");
 		}
-		else {
-			location.textArea.append("Maze completion unsuccessful \n");
-		}
-		
-		
-		
-		
-		
-		
-		
+			else {
+				location.textArea.append("Maze completion unsuccessful. \n");
+			}
 	}
 }
-	
-
